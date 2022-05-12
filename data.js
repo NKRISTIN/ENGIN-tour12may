77a7d8221419c49602f90ad59636af9f4eb84417
NKRISTIN <105353598+NKRISTIN@users.noreply.github.com>
2022-05-12 15:09:09 +0800

    EGIN WTM Summary
    My very first upload of a tour!

---
+++ data.js
@@ -0,0 +1,146 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-engin-drive-1",
+      "name": "ENGIN Drive 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.18515157735873444,
+        "pitch": 0.06355872545073638,
+        "fov": 1.4053540405216527
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9966215856883949,
+          "pitch": -0.11237138830562188,
+          "rotation": 1.5707963267948966,
+          "target": "1-engin-audi-entrance"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.7635275008393343,
+          "pitch": -0.2559089482871606,
+          "title": "Hi everyone!",
+          "text": "Welcome to the the start our ENGIN tour, click around to explore!"
+        }
+      ]
+    },
+    {
+      "id": "1-engin-audi-entrance",
+      "name": "ENGIN Audi Entrance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.20073258004988226,
+        "pitch": -0.5526015496944616,
+        "fov": 1.4053540405216527
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0437796833328061,
+          "pitch": -0.11505507988446695,
+          "rotation": 1.5707963267948966,
+          "target": "2-engin-audi-atrium"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.2815795264365768,
+          "pitch": -0.45955232235213117,
+          "title": "Introducing...the entrance to our school's auditorium! Isn't it just magnificent?",
+          "text": "Here we sit and stare at our&nbsp; floating dreams and ponder about whether we will make history."
+        }
+      ]
+    },
+    {
+      "id": "2-engin-audi-atrium",
+      "name": "ENGIN Audi Atrium",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.15602353824387372,
+        "pitch": -0.6880194061179985,
+        "fov": 1.4053540405216527
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0590584104188974,
+          "pitch": -0.3342721849879702,
+          "rotation": 20.420352248333668,
+          "target": "0-engin-drive-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.852399756896439,
+          "pitch": -0.4194815043359874,
+          "title": "Moving on to the Auditorium's Atrium space",
+          "text": "Here is where we sit and ponder if we should lie down and sleep instead."
+        }
+      ]
+    }
+  ],
+  "name": "ENGIN WTM Ks",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
